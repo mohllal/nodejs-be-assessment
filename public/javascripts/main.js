07c3eec277c3a6527bc4585d441a314fc4a17d5a
Kareem Mollal <kareem.mohllal@gmail.com>
2020-03-29 03:28:32 +0200

    Add UI for sorting

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,19 @@
 (function filter() {
   const $gallery = $('#gallery'); // Get the gallery element
-  const $search = $('#search'); // Get the input element
-  const $type = $('#type'); // Get the select element
+  const $searchText = $('#search-text'); // Get the search input element
+  const $searchType = $('#search-type'); // Get the search select element
+  const $sortType = $('#sort-type'); // Get the sort select element
 
   // Declare filterArray() function
   function filterResults() {
-    const query = $search.val().trim().toLowerCase(); // Get the query
-    const type = $type.val(); // Get the type
+    const query = $searchText.val().trim().toLowerCase(); // Get the search query
+    const searchType = $searchType.val(); // Get the search type
+    const sortType = $sortType.val();
+
+    console.log(`http://localhost:8088/api/v1/search?q=${query}&field=${searchType}&sortBy=${sortType}&page=1`);
 
     $.ajax({
-      url: `http://localhost:8088/api/v1/search?q=${query}&field=${type}&page=1`,
+      url: `http://localhost:8088/api/v1/search?q=${query}&field=${searchType}&sortBy=${sortType}&page=1`,
       type: 'GET',
       success(result) {
         console.log('Response:', result);
@@ -33,6 +37,7 @@
     });
   }
 
-  $search.on('keyup', filterResults); // Use keyup event to call filterResults()
-  $type.on('change', filterResults);// Use change event to call filterResults()
+  $searchText.on('keyup', filterResults); // Use keyup event to call filterResults()
+  $searchType.on('change', filterResults);// Use change event to call filterResults()
+  $sortType.on('change', filterResults);// Use change event to call filterResults()
 }());
